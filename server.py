72065fc5d148efc93b47c312248a6545b60f781c
Kate Schlough <krschlough@gmail.com>
2020-10-07 15:38:23 +0000

    Created seed_database.py to seed the database

--- server.py
+++ server.py
@@ -4,9 +4,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/'):
 
-# Replace this with routes and view functions!
+@app.route('api/')
 
+
+# crud: 
+# create_user(name)
+# create_recommendation(length, location, genre)
+# create_genre(genre_name)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
