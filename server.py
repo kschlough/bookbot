bf8320a91f6859f5002d0d7376faf86ac06d89c6
Kate Schlough <krschlough@gmail.com>
2020-10-08 19:59:04 +0000

    Installed Python Goodreads library

--- server.py
+++ server.py
@@ -1,11 +1,20 @@
 """Server for Bookbot recommendations app."""
 
 from flask import Flask
+import os
 
 app = Flask(__name__)
+SECRET_KEY = os.environ["SECRET_KEY"]
 
+# this is the homepage, for now it will have the form
 @app.route('/'):
+def form_fillout():
+    """Fill out the form to request a recommendation."""
 
+# this is where the recommendation will be returned to, and where the form will submit to
+@app.route('/recommendation'):
+
+# this is where the jsonify'd response from goodreads will be
 @app.route('api/')
 
 
