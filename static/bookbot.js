522b395aad4c53c42816cbf18e4d6a8fa8b94269
Kate Schlough <krschlough@gmail.com>
2020-10-21 21:58:41 +0000

    Added bookbot.js file and recommendation.html file

---
+++ static/bookbot.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// define the functions
+// all inside callback=initRec so it can be called
+function initRec() {
+    
+    const basicRec = () => {
+
+    }
+
+    // event handlers
+
+
+
+}
+
+
+
+
+
+
+
