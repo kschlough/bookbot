2c44f563b9f22dc0a533040df319c32ea4a0886b
Kate Schlough <krschlough@gmail.com>
2020-11-17 01:54:52 +0000

    Added js script src links to recommendation & homepage .jsx files in the respective html files

---
+++ static/recommendation.jsx
@@ -0,0 +1,11 @@
+// test - single page for form submit
+// // listen for form button click
+function submitForm() {
+    return (
+        <React.Fragment>
+            
+
+        </React.Fragment>
+    );
+}
+
