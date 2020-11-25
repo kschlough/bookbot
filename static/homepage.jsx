2993515b995afeab3bac250d90be4e0c7fa094ec
Kate Schlough <krschlough@gmail.com>
2020-11-25 17:40:13 +0000

    Added homepage.jsx

---
+++ static/homepage.jsx
@@ -0,0 +1,24 @@
+function Homepage() {
+    return (
+        <React.Fragment>
+            <h1>Welcome to Bookbot</h1>
+            <h2>Would you like to:</h2>
+            <ul>
+                <li>
+                    <a href='/form'>Request a recommendation</a>
+                </li>
+                
+                <p>or:</p>
+                
+                <li>
+                    <a href='/recent-requests'>View all of Bookbot's previous recommendations</a>
+                </li>
+            </ul>
+            
+        
+        </React.Fragment>
+    );
+};
+
+
+ReactDOM.render(<Homepage />, document.querySelector('#root'));
