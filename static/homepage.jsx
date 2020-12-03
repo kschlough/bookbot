6503909a700421b688e506ced2c0c3e90b4152a2
Kate Schlough <krschlough@gmail.com>
2020-12-03 03:09:24 +0000

    && more styling

--- static/homepage.jsx
+++ static/homepage.jsx
@@ -5,41 +5,35 @@ function Homepage() {
 
             <div className="homepage-container">
 
-                <div className="row" id="image-row">
-                    <div className="col">
                         
-                        <div className="homepage" id="homepage-img">
+                <div className="homepage" id="homepage-img">
 
-                            <div className="homepage">
+                    <div className="homepage">
 
-                                <div className="homepage-text">
-                                    <h1 id="welcome">Welcome to Bookbot!</h1>
-                                    <h2 id="choice">Would you like to:</h2>
+                        <div className="homepage-text">
+                            <h1 id="welcome">Welcome to Bookbot!</h1>
+                            <h2 id="choice">Would you like to:</h2>
 
-                                    <ul id="homepage-options">
-                                        <li>
-                                            <i className="fas fa-book"></i>
-                                            <a href='/form'> Request a recommendation</a>
-                                        </li>
-                                    
-                                    
-                                        <li>
-                                            <i className="fas fa-bookmark"></i>
-                                            <a href='/recent-requests'> View all of Bookbot's previous recommendations</a>
-                                        </li>
-                                    </ul>
+                            <ul id="homepage-options">
+                                <li>
+                                    <i className="fas fa-book"></i>
+                                    <a href='/form'> Request a recommendation</a>
+                                </li>
+                            
+                            
+                                <li>
+                                    <i className="fas fa-bookmark"></i>
+                                    <a href='/recent-requests'> View all of Bookbot's previous recommendations</a>
+                                </li>
+                            </ul>
 
-                                </div>
-
-                            </div>
                         </div>
+
                     </div>
+
+                </div>
                 </div> 
-            </div>
-
-
-   
-            
+           
         
         </React.Fragment>
     );
