a8a317fc3171d0b1662f9db60b350d322be887f7
Kate Schlough <krschlough@gmail.com>
2020-12-04 16:25:44 +0000

    More styling

--- static/homepage.jsx
+++ static/homepage.jsx
@@ -13,16 +13,19 @@ function Homepage() {
                         <h2>Would you like to:</h2>
 
                         <ul id="homepage-options">
+                            <h5>
                             <li>
                                 <i className="fas fa-book"></i>
                                 <a href='/form'> Request a recommendation</a>
                             </li>
+                            </h5>
                         
-                        
+                            <h5>
                             <li>
                                 <i className="fas fa-bookmark"></i>
                                 <a href='/recent-requests'> View all of Bookbot's previous recommendations</a>
                             </li>
+                            </h5>
                         </ul>
                         
                     </div>
